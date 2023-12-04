dde90e9c496a48138d1d97b8395dc6576a5fd576
axureworks <azurecode516@gmail.com>
2023-12-03 20:09:38 -0600

    added function declarations for hamming word decoding

--- hamming.h
+++ hamming.h
@@ -9,8 +9,24 @@
 #define _hamming_h
 
 #include <eigen3/Eigen/Dense>
+#include <iostream>
 
+/*! Decodes a Hamming word.
+* @param bits An int array containing the seven individual bits of the word
+*/
+void decodeHamming(const int (&bits)[7]);
 
+/*! Checks to see if an error is found in the bits.
+* @param hamMatrix An Eigen 1 x 7 int matrix containing the bits of the word.
+* @return The position in the matrix the error is at, if any. Returns 0 if there is no error.
+*/
+int hasError(const Eigen::MatrixXi &hamMatrix);
+
+/*! Fixes the error within the bits of the Hamming word, if needed.
+* @param hamMatrix An Eigen 1 x 7 int matrix containing the bits of the word.
+* @return The fixed version of the input matrix.
+*/
+Eigen::MatrixXi fixHammingWord(Eigen::MatrixXi hamMatrix);
 
 
 #endif
