6b1776e60d9740c5f876730f2297246656e24ca7
Cutter Crane <clgcrane@uab.edu>
2023-12-04 18:00:15 -0600

    the fixHam function isnt testing right not sure if test or other

--- unitTests.cpp
+++ unitTests.cpp
@@ -14,7 +14,11 @@ int main(){
         {0}
     };
     assert(("fillMatrix 1", fillMatrix(bits) == fillTest ));
-    assert(("hasError 1 error test", hasError(fillTest)));
+    assert(("hasError 1 error test", hasError(fillTest) == 2));
+    int bits1[7]{1,1,1,0,0,0,0};
+    
+    assert(("hasError 0 errors", hasError(fillMatrix(bits1))==0));
 
+    assert(("fixHamming 1 error",(fixHammingWord(fillTest,hasError(fillTest))==fillMatrix({0,0,1,0,1,1,0}))));
     std::cout << "all tests successful\n";
 }
